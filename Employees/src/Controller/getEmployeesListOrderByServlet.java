8a5f5eaf2215612c4de4ef87f82d515dec884b90
GD3 <GD3@DESKTOP-2OD8OMG>
2019-09-23 17:12:47 +0900

    EmployeesListOrderByServlet 추가 / EmployeesDao에 정렬 메소드 추가(오름 ,내림차순) GetEmployeesOrderBy.jsp 추가 / 오름 , 내림차순으로 (first_name) 페이지출력 , css table , thead 적용 , h1태그및 내용 text-center 적용

---
+++ Employees/src/Controller/getEmployeesListOrderByServlet.java
@@ -0,0 +1,31 @@
+package Controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import vo.*;
+import model.*;
+import java.util.*;
+
+@WebServlet("/employees/EmployeesListOrderBy")
+public class getEmployeesListOrderByServlet extends HttpServlet {
+	private EmployeesDao employeesDao ;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		employeesDao = new EmployeesDao();
+		
+		String order = request.getParameter("order");
+		
+		List<Employees> list = employeesDao.selectEmployeesListOrderBy(order);
+		 
+		request.setAttribute("list", list); //1 = list라 부르고 2 = list값을 request안에 복사
+		
+		System.out.println("getEmployeesListOrderByServlet param order :" + order); //값이 넘어왔는지 확인
+		
+		request.getRequestDispatcher("/WEB-INF/views/employees/EmployeesListOrderBy.jsp").forward(request, response); //forward = (1,2) 값을 ("/")으로 보내줌
+	}
+
+}
