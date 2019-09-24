f9ece162fcd3bdbb06b1de105ea83d86014f9c6b
GD3 <GD3@DESKTOP-2OD8OMG>
2019-09-24 17:18:44 +0900

    Dao -> 부서 번호 , 부서 이름 , 부서인원 출력메소드 추가

--- Employees/src/model/DepartmentsDao.java
+++ Employees/src/model/DepartmentsDao.java
@@ -9,6 +9,41 @@ public class DepartmentsDao {
 	public DepartmentsDao(){ //생성자 선언
 		
 	}
+	//부서 이름 , 부서 번호 , 부서 인원 출력 메소드
+		public List<Map<String , Object>> selectDepartmentsCountByDeptNo() {
+			List<Map<String , Object>> list = new ArrayList<Map<String , Object>>();
+			Connection conn = null;
+			PreparedStatement stmt = null;
+			ResultSet rs = null;
+			String sql = "SELECT d.dept_no dno , d.dept_name dna , COUNT(d.dept_no) cnt "
+					+ "FROM dept_emp de "
+					+ "INNER JOIN departments d ON de.dept_no = d.dept_no "
+					+ "WHERE de. to_date = '9999-01-01' "
+					+ "GROUP BY d.dept_no "
+					+ "ORDER BY COUNT(d.dept_no) desc ";
+			
+			try {
+				conn = DBHelper.getConnection();
+				stmt = conn.prepareStatement(sql);
+				rs = stmt.executeQuery();
+				while(rs.next()) {
+					Map<String , Object> map = new HashMap<String , Object>();
+						map.put("dno",rs.getString("dno"));
+						map.put("dna",rs.getString("dna"));
+						map.put("cnt",rs.getInt("cnt"));
+						list.add(map);
+					}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				DBHelper.close(rs, stmt, conn);
+			}
+			return list;
+			
+		}
+	
+	
+	
 	//전체 행을 출력하는 메소드
 	public int selectDepartmentsRowCount(){
 		int count = 0;
